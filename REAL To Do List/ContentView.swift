1d89b98320ba4b7b40aa25018a1fbd3be53d452b
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-28 11:03:59 -0500

    Initial Commit

---
+++ REAL To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REAL To Do List
+//
+//  Created by Yashwanth Ravipati on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ REAL To Do List/REAL_To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  REAL_To_Do_ListApp.swift
+//  REAL To Do List
+//
+//  Created by Yashwanth Ravipati on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct REAL_To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
