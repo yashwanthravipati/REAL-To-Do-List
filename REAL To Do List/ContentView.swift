8bdbdd4ca5706e2546addb1fa12fe476ff613251
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-28 11:08:53 -0500

    Set up list edit functionality

--- REAL To Do List/ContentView.swift
+++ REAL To Do List/ContentView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        NavigationView {
+                List {
+                        ForEach(things, id: \.self) { thing in
+                                            Text(thing)
+                                        }
+                        .onMove(perform: { indices, newOffset in
+                                           things.move(fromOffsets: indices, toOffset: newOffset)
+                                       })
+                        .onDelete(perform: { indexSet in
+                                           things.remove(atOffsets: indexSet)
+                                       })
+
+                    }
+                    .navigationBarTitle("Things", displayMode: .inline)
+                    .navigationBarItems(leading: EditButton())
+                }
+            }
+        }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
