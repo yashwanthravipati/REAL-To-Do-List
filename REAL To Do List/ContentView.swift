defe27dee04c25ee620905fa02ff4ea47b198834
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-28 11:29:15 -0500

    Created custom ToDoList class

--- REAL To Do List/ContentView.swift
+++ REAL To Do List/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var toDoItems =
-        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    @ObservedObject var toDoList = ToDoList()
     var body: some View {
         NavigationView {
             List {
-                ForEach(toDoItems) { item in
+                ForEach(toDoList.items) { item in
                     HStack {
                         VStack(alignment: .leading) {
                             Text(item.priority)
@@ -27,10 +24,10 @@ struct ContentView: View {
                     }
                 }
                 .onMove(perform: { indices, newOffset in
-                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    toDoList.items.move(fromOffsets: indices, toOffset: newOffset)
                 })
                 .onDelete(perform: { indexSet in
-                    toDoItems.remove(atOffsets: indexSet)
+                    toDoList.items.remove(atOffsets: indexSet)
                 })
             }
             .navigationBarTitle("To Do List", displayMode: .inline)
---
+++ REAL To Do List/ToDoList.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoList.swift
+//  REAL To Do List
+//
+//  Created by Yashwanth Ravipati on 7/28/21.
+//
+
+import Foundation
+
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
